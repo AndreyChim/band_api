e5b85912e2fc801acaf42c730887e59c981a0b82
AndreyChim <chimitcyrenov@mail.ru>
2021-08-03 15:50:11 +0800

    update versin ruby  & bands_controller.rb

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -5,7 +5,11 @@ class BandsController < ApplicationController
   def index
     @bands = Band.all
 
-    render json: @bands.include(:members), only: [:name]
+    #render json: @bands.include(:members), only: [:name]
+    
+    render json: @bands.as_json(include: :members, only: [:name])
+    
+    #render json: @bands, only: [:name]
   end
 
   # GET /bands/1
